54dbd5ec9dfd2858d09f3a09b5f43d5c63a948b0
berki <berkay.demiroren@gmail.com>
2022-10-07 00:33:48 +0300

    Fix story state

--- lib/bloc/story_state.dart
+++ lib/bloc/story_state.dart
@@ -1,10 +1,34 @@
 part of 'story_bloc.dart';
 
 abstract class StoryState extends Equatable {
-  const StoryState();
+  final List<StoryGroup> storyGroups;
+  final int currStoryIndex;
+  final int currStoryGroupIndex;
+
+  StoryState(this.storyGroups, this.currStoryGroupIndex, this.currStoryIndex);
+
+  copyWith({List<StoryGroup>? storyGroup, int? currStoryIndex, int? currStoryGroupIndex});
+
+  @override
+  List<Object> get props => [storyGroups, currStoryIndex, currStoryGroupIndex];
 }
 
 class StoryInitial extends StoryState {
+  StoryInitial() : super(storyGroups, 0, 0);
+
   @override
-  List<Object> get props => [];
+  copyWith({List<StoryGroup>? storyGroup, int? currStoryIndex, int? currStoryGroupIndex}) {}
+}
+
+class StoryUpdated extends StoryState {
+  StoryUpdated(
+    super.storyGroup,
+    super.currStoryGroupIndex,
+    super.currStoryIndex,
+  );
+
+  @override
+  StoryUpdated copyWith({List<StoryGroup>? storyGroup, int? currStoryIndex, int? currStoryGroupIndex}) {
+    return StoryUpdated(storyGroup ?? this.storyGroups, currStoryIndex ?? this.currStoryIndex, currStoryGroupIndex ?? this.currStoryGroupIndex);
+  }
 }
