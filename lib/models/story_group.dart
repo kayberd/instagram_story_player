739823632360a0055d4ddae9decaac53fda6d38b
berki <berkay.demiroren@gmail.com>
2022-10-06 11:08:28 +0300

    Add bloc

---
+++ lib/models/story_group.dart
@@ -0,0 +1,9 @@
+import 'package:instagram_story_player/models/story.dart';
+
+class StoryGroup {
+  final List<Story> stories;
+  final int user;
+
+  StoryGroup({required this.stories, required this.user});
+
+}
