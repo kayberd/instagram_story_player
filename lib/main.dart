4f56b0bbae6cfa6586d0946e8b456f2e163ea591
berki <berkay.demiroren@gmail.com>
2022-10-07 22:40:42 +0300

    Add main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:instagram_story_player/assets/media_data.dart';
-import 'package:instagram_story_player/screens/story_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:instagram_story_player/bloc/story_bloc.dart';
+import 'package:instagram_story_player/data/media_data.dart';
+import 'package:instagram_story_player/screens/story_group_screen.dart';
+import 'package:instagram_story_player/screens/story_groups_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  BlocOverrides.runZoned(() => runApp(const MyApp()), blocObserver: StoryBlocObserver());
 }
 
 class MyApp extends StatelessWidget {
@@ -13,12 +16,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: StoryScreen(stories: stories),
-    );
+        home: BlocProvider<StoryBloc>(
+      create: (_) => StoryBloc(),
+      child: StoryGroupsScreen(),
+    ));
   }
 }
 
+class StoryBlocObserver extends BlocObserver {
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    print('Bloc: $bloc');
+    super.onTransition(bloc, transition);
+  }
+
+  @override
+  void onCreate(BlocBase bloc) {
+    print('Bloc Created');
+    super.onCreate(bloc);
+  }
+}
